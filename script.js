80fc08b704000fa7c67a0f7c6f2522e0c595c509
Oleg <alligator2d@rambler.ru>
2022-11-17 11:24:53 +0300

    four commit

--- script.js
+++ script.js
@@ -1,37 +1,40 @@
 'use strict';
 
-let numberOfFilms;
-function start() {
-  numberOfFilms = +prompt('How much a films you was watched?', '');
-
-  while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-    numberOfFilms = +prompt('How much a films you was watched?', '');
-  }
-}
-start();
+// let numberOfFilms;
+// personalMovieDB.start();
 const personalMovieDB = {
-  count: numberOfFilms,
+  count: 0,
   movies: {},
   actors: {},
   genres: [],
   private: false,
-};
+  rememberMyFilms: function () {
+    for (let i = 0; i < 2; i++) {
+      const a = prompt('Last film you watched?', '').trim(),
+        b = prompt('How rating is this film?', '');
 
-function rememberMyFilms() {
-  for (let i = 0; i < 2; i++) {
-    const a = prompt('Last film you watched?', '').trim(),
-      b = prompt('How rating is this film?', '');
-
-    if (a !== null && b !== null && a !== '' && b !== '' && a.length < 50) {
-      personalMovieDB[a] = b;
-      console.log('done');
-    } else {
-      console.log('Sorry');
-      i--;
+      if (a !== null && b !== null && a !== '' && b !== '' && a.length < 50) {
+        personalMovieDB[a] = b;
+        console.log('done');
+      } else {
+        console.log('Sorry');
+        i--;
+      }
     }
-  }
-}
-rememberMyFilms();
+  },
+  start: function () {
+    numberOfFilms = +prompt('How much a films you was watched?', '');
+
+    while (
+      numberOfFilms == '' ||
+      numberOfFilms == null ||
+      isNaN(numberOfFilms)
+    ) {
+      numberOfFilms = +prompt('How much a films you was watched?', '');
+    }
+  },
+};
+personalMovieDB.rememberMyFilms();
 
 function detectPersonalLevel() {
   if (numberOfFilms < 10) {
@@ -64,51 +67,3 @@ function writeYourGenres() {
 }
 writeYourGenres();
 // console.log(personalMovieDB);
-
-const usdCurr = 28;
-const discount = 0.9;
-function convert(amount, curr) {
-  return amount * curr;
-}
-function promotion(result) {
-  console.log(result * discount);
-}
-// convert(30, usdCurr);
-promotion(convert(500, usdCurr));
-
-function test() {
-  for (let i = 0; i < 5; i++) {
-    console.log(i);
-    if (i === 3) {
-      return;
-    }
-  }
-  console.log('hello');
-}
-test();
-
-function SayHello(name) {
-  console.log(`Hello ${name}!!`);
-}
-SayHello('Alligator');
-
-function returnNeighboringNumbers(num) {
-  return [num - 1, num, num + 1];
-}
-console.log(returnNeighboringNumbers(5));
-
-function getMathResult(a, b) {
-  if (typeof b !== 'number' || b <= 0) {
-    return a;
-  }
-  let result = '';
-  for (let i = 1; i <= b; i++) {
-    if (i === b) {
-      result += `${a * i}`;
-    } else {
-      result += `${a * i}--`;
-    }
-  }
-  console.log(result);
-}
-getMathResult(5, 3);
